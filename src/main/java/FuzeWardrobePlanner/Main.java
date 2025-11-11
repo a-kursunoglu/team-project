c9fd648bbe06d0bd105d1d4e9e5e0f27f8e6894e
jadenal-aidroos <jjtortoise@icloud.com>
2025-11-11 04:16:37 -0500

    Hopefully about to merge

--- src/main/java/FuzeWardrobePlanner/Main.java
+++ src/main/java/FuzeWardrobePlanner/Main.java
@@ -5,6 +5,6 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) {
-       // Do whatever you want here until we implement the final stuff
+       // Do whatever you want here until we implement the final stuff, yay
     }
 }
