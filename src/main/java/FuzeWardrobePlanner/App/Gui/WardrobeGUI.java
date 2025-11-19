9b8668598adc0a9ffb48301590f8adf3b6295e8d
Chang Yeyun <yeyun.chang05@gmail.com>
2025-11-18 22:07:40 -0500

    deleteconfirm #18

---
+++ src/main/java/FuzeWardrobePlanner/App/Gui/WardrobeGUI.java
@@ -0,0 +1,46 @@
+package FuzeWardrobePlanner.App;
+
+import FuzeWardrobePlanner.App.Gui.WardrobePanel;
+import FuzeWardrobePlanner.Entity.Clothing.ClothingArticle;
+import FuzeWardrobePlanner.Entity.Clothing.Photo;
+import FuzeWardrobePlanner.Entity.Clothing.Wardrobe;
+import FuzeWardrobePlanner.UserCases.ManageWardrobeInteractor;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WardrobeGUI {
+    public static void main(String[] args) {
+
+        Wardrobe wardrobe = new Wardrobe();
+
+        wardrobe.addItem(new ClothingArticle(
+                "Hoodie", "TOPS", 2, false, new Photo("placeholder.jpg")   // put real photo here later
+        ));
+        wardrobe.addItem(new ClothingArticle(
+                "Jacket", "OUTERWEAR", 3, true, new Photo("placeholder.jpg")
+        ));
+        wardrobe.addItem(new ClothingArticle(
+                "Jacket2", "OUTERWEAR", 3, true, new Photo("placeholder.jpg")
+        ));
+
+        ManageWardrobeInteractor interactor = new ManageWardrobeInteractor(wardrobe);
+
+        JFrame frame = new JFrame("My Wardrobe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        WardrobePanel wardrobePanel = new WardrobePanel(interactor);
+        frame.add(new JScrollPane(wardrobePanel), BorderLayout.CENTER);
+        JButton weatherButton = new JButton("View Today's Weather");
+        weatherButton.addActionListener(e -> {
+            new FuzeWardrobePlanner.View.WeatherGUI();
+        });
+
+        frame.add(weatherButton, BorderLayout.NORTH);
+
+        frame.setVisible(true);
+    }
+
+}
